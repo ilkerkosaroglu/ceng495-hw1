928943645ffe5a4fe1b73ca627df41469a7dd401
ilker <ilkerkosaroglu@gmail.com>
2023-04-23 07:07:37 +0300

    fixed zustand types and persistance

--- renderApp/src/state/userStore.ts
+++ renderApp/src/state/userStore.ts
@@ -1,10 +1,11 @@
-import { create } from 'zustand'
+import { StateCreator, create } from 'zustand';
+import { PersistOptions, persist } from 'zustand/middleware';
 import { User } from '.';
 
 const getUserInfo = () => {
     const userInfo = localStorage.getItem('userInfo');
     if (userInfo) {
-        return JSON.parse(userInfo);
+        return JSON.parse(userInfo) as User;
     }
     return null;
 };
@@ -13,11 +14,16 @@ const setUserInfo = (userInfo: User|null) => {
     localStorage.setItem('userInfo', JSON.stringify(userInfo));
 };
 
-export const useUserStore = create<{user:User|null, setUser:(user:User|null)=>void}>((set) => ({
+type StoreType = {user:User|null, setUser:(user:User|null)=>void};
+type PersistType = (
+      config: StateCreator<StoreType>,                                            
+      options: PersistOptions<StoreType>                                          
+    ) => StateCreator<StoreType> 
+export const useUserStore = create<StoreType>((persist as PersistType)((set) => ({
     user: getUserInfo(),
     setUser: (user) => {
         set({ user });
         setUserInfo(user);
-    },
-}));
+    }
+}),{name: 'usrInfo'}));
 
